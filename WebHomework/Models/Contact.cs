93f3dbfac2146f2761fc520f1c7c8e4ff88807e1
Vladimir Grigorov <vladiimirgrigorov@gmail.com>
2025-04-14 23:41:52 +0300

    Refactored Contact model

--- WebHomework/Models/Contact.cs
+++ WebHomework/Models/Contact.cs
@@ -4,9 +4,9 @@
     {
         public int? Id { get; set; }
         public string? Name { get; set; }
-        public List<PhoneNumber>? PhoneNumbers { get; set; } = new List<PhoneNumber>();
+        public List<PhoneNumber> PhoneNumbers { get; set; } = new List<PhoneNumber>();
         public Address? Address { get; set; }
-        public DateTime? CreatedAt { get; set; }
-        public DateTime? UpdatedAt { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
     }
 }
