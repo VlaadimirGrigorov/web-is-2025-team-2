48ba3c83353d26bf7f8a080caa29ba06272c3291
Vladimir Grigorov <vladiimirgrigorov@gmail.com>
2025-04-27 14:33:03 +0300

    Added all changes

---
+++ WebHomework/DTOs/ContactResponseDto.cs
@@ -0,0 +1,12 @@
+﻿using WebHomework.Models;
+
+namespace WebHomework.DTOs
+{
+    public class ContactResponseDto
+    {
+        public int? Id { get; set; }
+        public string? Name { get; set; }
+        public AddressDto? Address { get; set; }
+        public List<PhoneNumberResponseDto> PhoneNumbers { get; set; } = new List<PhoneNumberResponseDto>();
+    }
+}
